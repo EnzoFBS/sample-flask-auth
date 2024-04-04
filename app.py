b68ed63838903a72d0222880a70bef22e4f8ef43
EnzoFBS <enzoplay47@gmail.com>
2024-04-04 12:12:04 -0300

    implemento login

--- app.py
+++ app.py
@@ -1,11 +1,41 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, jsonify
+from models.user import User
+from database import db
+from flask_login import LoginManager, login_user, current_user
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+login_manager = LoginManager()
 
-db = SQLAlchemy(app)
+
+db.init_app(app)
+login_manager.init_app(app)
+#Session <- conexão ativa
+
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+@app.route('/login', methods=['POST'])
+def login():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+  
+  if username and password:
+      user = User.query.filter_by(username=username).first()
+      
+      if user and user.password == password:
+          login_user(user)
+          print(current_user.is_authenticated)
+          return jsonify({"Message": "Sucess"}) , 200
+      
+  
+  return jsonify({"Message": "Credencias invalidas"})   
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
